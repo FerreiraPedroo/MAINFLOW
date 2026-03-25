6fd0978655e63257e8b96887fd2df48c40cbda1c
FerreiraPedroo <pedroohenrique@hotmail.com>
2026-03-25 18:01:18 -0300

    update

---
+++ src/features/operations/maintenance/maintenance-pages.ts
@@ -0,0 +1,19 @@
+import * as maintenance from "./index";
+
+export const maintenanceProjectPages = [
+  // {
+  //   path: "/projects/:id",
+  //   element: projects.ProjectDetailsPage,
+  //   permission: "operations:projects",
+  // },
+  // {
+  //   path: "/projects",
+  //   element: projects.ProjectListPage,
+  //   permission: "operations:projects",
+  // },
+  // {
+  //   path: "/projects/create",
+  //   element: projects.ProjectCreatePage,
+  //   permission: "operations:projects",
+  // },
+];
